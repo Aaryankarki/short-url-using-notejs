74f8c61aafbb9d49469351905808b05c663b3cbe
Aaryan <169142512+Aaryankarki@users.noreply.github.com>
2025-07-25 13:32:56 +0545

    first commit

--- routes/url.js
+++ routes/url.js
@@ -2,7 +2,7 @@ const express=require('express');
 const {handleGenerateNewShortURL}=require('../controller/url');
 const router=express.Router();
 
-
+//handling the new url
 router.post("/",handleGenerateNewShortURL)
 
 module.exports=router;
